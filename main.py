4b0e06a76c7b8d5d8b5df67b0c0b37e89760612e
27freeman <96946280+27freeman@users.noreply.github.com>
2025-07-11 13:51:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tweepy,json,re,logging,os
-from datetime import datetime,timezone,timedelta
+import requests
+import logging,os
+from datetime import datetime
 from fastapi import FastAPI
 
 logging.basicConfig(
@@ -7,97 +8,77 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
-# with open('key.json','r') as file:
-#     keys = json.load(file)
-#     bearerToken =keys['bearerToken']
-
-bearerToken = os.environ.get('bearerToken')
-client = tweepy.Client(bearer_token=bearerToken)
+API_KEY =  os.environ.get('ApiKey')
+BASE_URL = "https://api.twitterapi.io/twitter/tweet/advanced_search"
 app = FastAPI()
 
+class TweetSearch:
+    def __init__(self,api_key):
+        self.header = {
+            "X-API-Key": api_key
+        }
+        self.params = {
+            "query": '',  
+            'cursor':"",
+            'hash_next_page':True
+            }
+        self.all_tweets = []
+        self.EarlyTweets = []
 
 
-def checkDuplicateUser(user_tweet,username,tweet_date):
-    from datetime import datetime
-    add = True
+searcher = TweetSearch(API_KEY)
+def search():
     try:
-        for index,tweet in enumerate(user_tweet):
-            if tweet['username'] == username and datetime.strptime(tweet['created_at'],"%Y-%m-%d %H:%M") > datetime.strptime(tweet_date,"%Y-%m-%d %H:%M"):
-                del user_tweet[index]
-                add = True
+        while True:
+            # Make the GET request to the TwitterAPI.io endpoint
+            response = requests.get(BASE_URL, headers=searcher.header, params=searcher.params)
+            data = response.json()
+            tweets = data.get('tweets', [])
+            if not tweets:
+                logging.info("No tweets found for the given query. Checking Next Hours Tweets")
                 break
-            elif tweet['username'] == username and datetime.strptime(tweet['created_at'],"%Y-%m-%d %H:%M") < datetime.strptime(tweet_date,"%Y-%m-%d %H:%M"):
-                add = False
+            for tweet in tweets:
+                tweet_info = {
+                    "userName": tweet.get("userName"),
+                    "text": tweet.get("text"),
+                    "createdAt": tweet.get("createdAt"),
+                    'tweet_link': tweet.get('url')
+                }
+                searcher.all_tweets.append(tweet_info)
+            if not data['next_cursor']:
+                logging.warning("No more tweets found or reached the end of results.")
                 break
-            else:
-                add = True
-    except Exception as e:
-        add = True
-    return user_tweet, add
 
+            searcher.params['cursor'] = data['next_cursor']
+            logging.info(f"Fetched {len(data['tweets'])} tweets, moving to next page...") 
+    except requests.exceptions.HTTPError as http_err:
+        logging.error(f"HTTP error occurred: {http_err}")
+    except requests.exceptions.RequestException as err:
+        logging.error(f"Error occurred: {err}")
+    except ValueError as json_err:
+        logging.error(f"Error parsing JSON response: {json_err}")
 
-
-def TweetSearch(keywords:str,date):
-    users_tweet = []
-    try:
-        for response in tweepy.Paginator(client.search_recent_tweets,
-                                        keywords,
-                                        start_time=date,
-                                        max_results=100,
-                                        limit=5,
-                                        user_fields=['public_metrics','username'],
-                                        tweet_fields=['author_id','created_at','public_metrics'],
-                                        expansions=['author_id']
-                                        ):
-            user_map = {user.id: user for user in response.includes.get('users', [])}
-            for tweet in response.data:
-                user = user_map.get(tweet.author_id)
-                if user:
-                    metrics = user.public_metrics
-                    username = user.username
-                    follower_count = metrics['followers_count']
-                else:
-                    continue
-                tweet_date = tweet.created_at.strftime("%Y-%m-%d %H:%M")
-                parsed_time = datetime.strptime(tweet_date,"%Y-%m-%d %H:%M")
-                new_time = parsed_time + timedelta(hours=1)
-                tweet_date = new_time.strftime("%Y-%m-%d %H:%M")
-                # Flter account with low followesr account Out
-                follower_threshold = 100
-                if int(follower_count) < int(follower_threshold):
-                    continue
-                users_tweet,affirm = checkDuplicateUser(users_tweet,username,tweet_date)
-                if affirm is True:
-                    tweet_dict = {
-                            'Username': username,
-                            'Followers':follower_count,
-                            'Tweeted_date':tweet_date,
-                            'Tweet_text': re.sub(r'\n+','. ',tweet.text),
-                            'Tweet_link': f'https://x.com/{username}/status/{tweet.id}'
-                            }
-                    users_tweet.append(tweet_dict)
-    except Exception as e:
-        logging.error(f'Error Spoted issue:{e}')
-        return e
-    return users_tweet
-
-@app.get('/search/{query}/{date}')
-def search(query:str,date:str,limit:int = 10,checkAlive:bool = False):
+@app.get('/search/{keyword}/{date}')
+def search_tweets(keyword:str,date:str,limit:int = 1,checkAlive:bool = False):
     if checkAlive:
         logging.info('Checking if Api is Alive')
         return {'Status':200}
-    logging.info('Requesting For Data')
-    dt = datetime.fromisoformat(date)
-    dt_utc = dt.replace(tzinfo=timezone.utc)
-    print(dt_utc)
-    date = dt_utc.strftime('%Y-%m-%dT%H:%M:%SZ')
-    SearchedResult = TweetSearch(query,date)
-    RequestedResult = list()
-    for result in reversed(SearchedResult):
-
-        if len(RequestedResult) == limit:
+    hour = 0
+    while True:
+        hour += 1
+        keyword_date = f"{keyword} until:{date}_{hour}:00:00_UTC"
+        searcher.params['query'] = keyword_date
+        search()
+        if searcher.all_tweets:
+            logging.info(f"Fetched {len(searcher.all_tweets)} tweets for keyword: {keyword_date}")
             break
-        RequestedResult.append(result)
-    logging.info('Retrieving Data')
-    return RequestedResult
+    for tweet in reversed(searcher.all_tweets):
+        if len(searcher.EarlyTweets) == limit:
+            break
+        searcher.EarlyTweets.append(tweet)
+    return searcher.EarlyTweets     
+
+
+
+
 
